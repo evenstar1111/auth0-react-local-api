5cb1df81e03e534382d3b09ac908d9c2fe3d1f64
sk shahil <skshahil35@gmail.com>
2020-09-25 19:10:51 +0530

    enabled CORS

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ const PORT = process.env.PORT || 7000;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+})
+
 require('./routes/get')(app);
 
 app.listen(PORT, console.log('server is on'));
